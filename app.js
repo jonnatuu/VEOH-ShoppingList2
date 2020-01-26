53b0df0525a4677f13bfc65710b1062055323c6e
Jonna Orava <jonnatuu@icloud.com>
2020-01-26 10:11:36 +0200

    commit

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const PORT = process.env.PORT || 8080;
+
+let app = express();
+
+app.use((req, res, next) => {
+    console.log('PATH: ' + req.path);
+    next();
+});
+
+app.get('/', (req, res, next) => {
+    res.send('Hello');
+    res.end();
+});
+
+app.use((req, res, next) => {
+    console.log('404');
+    res.status(404);
+    res.send('404');
+    res.end();
+});
+
+
+app.listen(PORT);
